24dfe1199b23eaf3d333c8cb2dd399159a2e5546
Rutuja Patil <rutuja.patil@contentstack.com>
2025-11-11 18:11:13 +0530

    layout added

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import './App.css'
 
 const App = (): React.ReactElement => {
   return (
-    <div className="text-primary">
-      Hello World
+    <div className="app">
+      <h1 className="header">Welcome to Lumini UI</h1>
+      <div className="container--chart">charts goes here</div>
+      <div className="container--table">table goes here</div>
     </div>
   );
 };
